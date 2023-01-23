019c843a07fd3fe22e2f9c09bc9c6a09a97ca732
Akash Tanwar <76595372+akashdiv@users.noreply.github.com>
2023-01-23 17:18:08 +0530

    add

---
+++ index.js
@@ -0,0 +1,38 @@
+import UI from './ui.js';
+import Task from './task.js';
+
+const ui = new UI();
+
+ui.showAllTasks();
+
+document.querySelector('.AddTaskBtn').addEventListener('click', e => {
+    const newTitle = document.querySelector('#newtaskID').value;
+    if(!newTitle) return;
+    const task = new Task(newTitle);
+
+    ui.addToUI(task);
+    ui.resetForm();   
+
+});
+
+document.querySelector('.task-list').addEventListener('click', (e) => {
+    if(e.target.className.includes('task__op_edit')){
+        ui.editTask(e);
+    }
+    if(e.target.className.includes('task__op_delete')){
+        ui.deleteTask(e);
+    }
+    if(e.target.className.includes('task-check')){
+        ui.completeTask(e);
+    }
+
+});
+
+document.querySelector('.EditTaskBtn').addEventListener('click', (e) => {
+    ui.updateTask(e);
+})
+
+document.querySelector('.CancelTaskBtn').addEventListener('click', (e) => {
+    ui.cancelTask(e);
+});
+
